2cc80f4408dbec4cd95d27e73816031ff36d3e33
Allen Yeh <allen_yeh@systemweb.com.tw>
2024-11-11 04:20:24 +0800

    🆙update: 更新 defaut layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,18 @@
+<script setup lang="ts">
+const props = defineProps({
+  class: {
+    type: String,
+  },
+})
+</script>
+
 <template>
   <div class="relative h-full grid">
     <BackDecoration />
-    <div class="slidev-layout">
-      <slot />
+    <div class="slidev-layout grid" :class="[props.class]">
+      <div class="my-auto">
+        <slot />
+      </div>
     </div>
   </div>
 </template>
