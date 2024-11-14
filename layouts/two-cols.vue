bde4418954bf46a87ec810a51757a483ec37d224
Allen Yeh <allen_yeh@systemweb.com.tw>
2024-11-14 11:00:20 +0800

    🆙update: 更新 two-cols layout

---
+++ layouts/two-cols.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = defineProps({
+  class: {
+    type: String,
+  },
+  verticalMiddle: {
+    type: Boolean,
+    default: true,
+  },
+})
+</script>
+
+<template>
+  <div class="relative h-full grid">
+    <BackDecoration />
+    <div class="slidev-layout two-columns w-full h-full grid grid-cols-2">
+      <div :class="[verticalMiddle ? 'my-auto' : '', props.class]">
+        <slot />
+      </div>
+      <div :class="[verticalMiddle ? 'my-auto' : '', props.class]">
+        <slot name="right" />
+      </div>
+    </div>
+  </div>
+</template>
